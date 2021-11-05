98ec156d24098eaa81e7656078f0ccdbd11e4bd9
Sergio2822 <bismark2822@gmail.com>
2021-11-05 10:52:39 -0600

    git PORT updated

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connection = require('./mongo/MongoClientFactory')
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 
 // Router
